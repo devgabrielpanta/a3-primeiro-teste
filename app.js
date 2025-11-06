58f55c656a564deabd32f0b79b96282de984fa84
devgabrielpanta <gabrielpanta.dev@gmail.com>
2025-11-06 11:52:31 +0000

    feat: setup inicial do projeto e primeiro console.log

---
+++ app.js
@@ -0,0 +1,10 @@
+// Passo 1: MOSTRAR a mensagem de boas-vindas
+console.log("Olá! Bem-vindos à programação!");
+
+// Passo 2: Mostrar a soma (Processamento Simples)
+console.log(10 + 5);
+
+// Passo 3: Mostrar um valor Booleano (revisão da Aula 1)
+console.log(true);
+
+console.log("hi there!")
